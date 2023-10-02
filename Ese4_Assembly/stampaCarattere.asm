592f45c8ace3de85c4de6b914f8a55fc2d9df867
Matthew Gasparetti <106164674+b3nj4m1no@users.noreply.github.com>
2023-10-02 10:29:42 +0200

    Update stampaCarattere.asm

--- Ese4_Assembly/stampaCarattere.asm
+++ Ese4_Assembly/stampaCarattere.asm
@@ -1,5 +1,10 @@
 ;Gasparetti
 
+;Scrivere un programma che carichi 
+;nella cella di memoria di indirizzo 200h 
+;un carattere letto da tastiera e, successivamente,
+;lo copi nella cella di memoria precedente e in quella successiva
+
 org 100h        ;inizio il programma all indirizzo 100h
 
 mov bx,200h     ;sono pronto per caricare il carattere all' indirizzo 200h
